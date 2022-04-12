ee8b2ff9d0f68daed180c03e74bb0733c1efb3a5
Tavin <99303252+Skelitin-JS@users.noreply.github.com>
2022-04-12 10:14:37 -0500

    hello const

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-const hello = 12 x= 2
+const hello = 12 * 3
+console.log (`${hello}`)
+/*this should output 36, simply just doing some basic javascript because I haven't done any for a while.
+this should also be a comment */
